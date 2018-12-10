f02c954b391907c64d11cc047a34b5945ae12c65
DashaMorozova <daria.morozova1@nure.ua>
2018-12-09 23:06:05 -1100

    Implements interface functions

---
+++ usermanagement/src/main/java/ua/nure/kn/morozova/usermanagement/gui/BrowsePanel.java
@@ -0,0 +1,167 @@
+package ua.nure.kn.morozova.usermanagement.gui;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import ua.nure.kn.morozova.usermanagement.db.DatabaseException;
+
+public class BrowsePanel extends JPanel implements ActionListener {
+
+	private MainFrame parent;
+	private JScrollPane tablePanel;
+	private JTable userTable;
+	private JPanel buttonsPanel;
+	private JButton addButton;
+	private JButton editButton;
+	private JButton deleteButton;
+	private JButton detailsButton;
+	private Long bufferedId;
+
+	public BrowsePanel(MainFrame mainFrame) {
+		parent = mainFrame;
+		initialize();
+	}
+
+	private void initialize() {
+		this.setName("browsePanel"); //$NON-NLS-1$
+		this.setLayout(new BorderLayout());
+		this.add(getTablePanel(), BorderLayout.CENTER);
+		this.add(getButtonsPanel(), BorderLayout.SOUTH);
+	}
+
+	private JPanel getButtonsPanel() {
+		if (buttonsPanel == null) {
+			buttonsPanel = new JPanel();
+			buttonsPanel.add(getAddButton());
+			buttonsPanel.add(getEditButton());
+			buttonsPanel.add(getDeleteButton());
+			buttonsPanel.add(getdDetailsButton());
+		}
+		return buttonsPanel;
+	}
+
+	private JButton getdDetailsButton() {
+		if (detailsButton == null) {
+			detailsButton = new JButton();
+			detailsButton.setText("Details"); //localize //$NON-NLS-1$
+			detailsButton.setName("detailsButton"); //$NON-NLS-1$
+			detailsButton.setActionCommand("details"); //$NON-NLS-1$
+			detailsButton.addActionListener(this);
+		}
+		return detailsButton;
+	}
+
+	private JButton getDeleteButton() {
+		if (deleteButton == null) {
+			deleteButton = new JButton();
+			deleteButton.setText("Delete"); //localize //$NON-NLS-1$
+			deleteButton.setName("deleteButton"); //$NON-NLS-1$
+			deleteButton.setActionCommand("delete"); //$NON-NLS-1$
+			deleteButton.addActionListener(this);
+		}
+		return deleteButton;
+	}
+
+	private JButton getEditButton() {
+		if (editButton == null) {
+			editButton = new JButton();
+			editButton.setText("Edit"); //localize //$NON-NLS-1$
+			editButton.setName("editButton"); //$NON-NLS-1$
+			editButton.setActionCommand("edit"); //$NON-NLS-1$
+			editButton.addActionListener(this);
+		}
+		return editButton;
+	}
+
+	private JButton getAddButton() {
+		if (addButton == null) {
+			addButton = new JButton();
+			addButton.setText("Add"); //localize //$NON-NLS-1$
+			addButton.setName("addButton"); //$NON-NLS-1$
+			addButton.setActionCommand("add"); //$NON-NLS-1$
+			addButton.addActionListener(this);
+		}
+		return addButton;
+	}
+
+	private JScrollPane getTablePanel() {
+		if (tablePanel == null) {
+			tablePanel = new JScrollPane(getUserTable());
+		}
+		return tablePanel;
+	}
+
+	public JTable getUserTable() {
+		if (userTable == null) {
+			userTable = new JTable();
+			userTable.setName("userTable"); //$NON-NLS-1$
+	
+		}
+		return userTable;
+	}
+
+	public void initTable() {
+		UserTableModel model;
+		try {
+			model = new UserTableModel(parent.getDao().findAll());
+		} catch (DatabaseException e) {
+			model = new UserTableModel(new ArrayList());
+			JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$
+		}
+		getUserTable().setModel(model);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String actionCommand = e.getActionCommand();
+		if("add".equalsIgnoreCase(actionCommand)) { //$NON-NLS-1$
+			this.setVisible(false);
+			parent.showAddPanel();
+		}
+		
+		if("edit".equalsIgnoreCase(actionCommand)) {	
+			if(userTable.getSelectedRow() == -1) {
+				JOptionPane.showMessageDialog(this, "Row was not selected", "Error", JOptionPane.ERROR_MESSAGE);
+			} else {
+				Long selectedUserId = (Long) userTable.getValueAt(userTable.getSelectedRow(), 0);
+				this.setVisible(false);
+				parent.showEditPanel(selectedUserId);
+			}
+		}
+		
+		if ("delete".equalsIgnoreCase(actionCommand)) {
+			if (userTable.getSelectedRow() == -1) {
+				JOptionPane.showMessageDialog(this, "Row was not selected", "Error", JOptionPane.ERROR_MESSAGE);
+			} else {
+				int choice = JOptionPane.showOptionDialog(null, "You really want to delete?", "Delete?",
+						JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+				if (choice == JOptionPane.YES_OPTION) {
+					try {
+						parent.getUserDao().delete(parent.getUserDao().find(bufferedId));
+					} catch (DatabaseException e1) {
+						JOptionPane.showMessageDialog(this, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+				initTable();
+			}
+		}
+
+		if ("details".equalsIgnoreCase(actionCommand)) {
+			if (userTable.getSelectedRow() == -1) {
+				JOptionPane.showMessageDialog(this, "Row was not selected", "Error", JOptionPane.ERROR_MESSAGE);
+			} else {
+				Long selectedUserId = (Long) userTable.getValueAt(userTable.getSelectedRow(), 0);
+				this.setVisible(false);
+				parent.showDetailsPanel(selectedUserId);
+			}
+		}
+	}
+}
---
+++ usermanagement/src/main/java/ua/nure/kn/morozova/usermanagement/gui/EditPanel.java
@@ -0,0 +1,184 @@
+package ua.nure.kn.morozova.usermanagement.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.ParseException;
+
+import ua.nure.kn.morozova.usermanagement.User;
+import ua.nure.kn.morozova.usermanagement.db.DatabaseException;
+import ua.nure.kn.morozova.usermanagement.util.Messages;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
+public class EditPanel extends JPanel implements ActionListener {
+
+	private Long userId;
+	private MainFrame parentFrame;
+	private JTextField firstNameField;
+	private JTextField lastNameField;
+	private JTextField dateOfBirthField;
+	private JPanel fieldPanel;
+	private JPanel buttonPanel;
+	private JButton okButton;
+	private JButton cancelButton;
+	private Long bufferedId;
+	
+	public EditPanel(MainFrame parent) {
+		this.parentFrame = parent;
+		initialize();
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if ("ok".equalsIgnoreCase(e.getActionCommand())) {
+			User user = new User();
+			user.setId(userId);
+			user.setFirstName(getFirstNameField().getText());
+			user.setLastName(getLastNameField().getText());
+			DateFormat format = DateFormat.getDateInstance();
+			try {
+				user.setDateOfBirth(format.parse(getDateOfBirthField().getText()));
+			} catch (ParseException e1) {
+				getDateOfBirthField().setBackground(Color.RED);
+				return;
+			} 
+			try {
+				parentFrame.getDao().update(user);
+			} catch (DatabaseException e1) {
+				JOptionPane.showMessageDialog(this, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$
+			}
+		}
+		
+		clearFields();
+		this.setVisible(false);
+		parentFrame.showBrowsePanel();
+	}
+
+
+    private void initialize() {
+        this.setName("editPanel");
+        this.setLayout(new BorderLayout());
+        this.add(getFieldPanel(), BorderLayout.NORTH);
+        this.add(getButtonsPanel(), BorderLayout.SOUTH);
+
+    }
+    
+	private JPanel getButtonsPanel() {
+		if (buttonPanel == null) {
+			buttonPanel = new JPanel();
+			buttonPanel.add(getOkButton(), null);
+			buttonPanel.add(getCancelButton(), null);
+
+		}
+		return buttonPanel;
+	}
+	
+	private JButton getCancelButton() {
+		if (cancelButton == null) {
+			cancelButton = new JButton();
+			cancelButton.setText(Messages.getString("AddPanel.cancel")); 
+			cancelButton.setName("cancelButton"); 
+			cancelButton.setActionCommand("cancel"); 
+			cancelButton.addActionListener(this);
+		}
+		return cancelButton;
+	}
+
+	private JButton getOkButton() {
+		if (okButton == null) {
+			okButton = new JButton();
+			okButton.setText(Messages.getString("AddPanel.ok")); 
+			okButton.setName("okButton"); 
+			okButton.setActionCommand("ok"); 
+			okButton.addActionListener(this);
+		}
+		return okButton;
+	}
+
+
+	
+	private JTextField getDateOfBirthField() {
+		if (dateOfBirthField == null) {
+			dateOfBirthField = new JTextField();
+			dateOfBirthField.setName("dateOfBirthField");
+		}
+		return dateOfBirthField;
+	}
+
+	private JTextField getLastNameField() {
+		if (lastNameField == null) {
+			lastNameField = new JTextField();
+			lastNameField.setName("lastNameField");
+		}
+		return lastNameField;
+	}
+
+	private JTextField getFirstNameField() {
+		if (firstNameField == null) {
+			firstNameField = new JTextField();
+			firstNameField.setName("firstNameField");
+		}
+		return firstNameField;
+	}
+	
+	private void addLabeledField(JPanel panel, String labelText, JTextField textField) {
+		JLabel label = new JLabel(labelText);
+		label.setLabelFor(textField);
+		panel.add(label);
+		panel.add(textField);
+
+	}
+	
+	private void clearFields() {
+		getFirstNameField().setText(""); 
+		getFirstNameField().setBackground(Color.white);
+
+		getLastNameField().setText(""); 
+		getLastNameField().setBackground(Color.white);
+
+		getDateOfBirthField().setText(""); 
+		getDateOfBirthField().setBackground(Color.white);
+
+	}
+	
+	private JPanel getFieldPanel() {
+		if (fieldPanel == null) {
+			fieldPanel = new JPanel();
+			fieldPanel.setLayout(new GridLayout(3, 2));
+			addLabeledField(fieldPanel, Messages.getString("AddPanel.firstname"), getFirstNameField()); //$NON-NLS-1$
+			addLabeledField(fieldPanel, Messages.getString("AddPanel.lastname"), getLastNameField()); //$NON-NLS-1$
+			addLabeledField(fieldPanel, Messages.getString("AddPanel.date_of_birth"), getDateOfBirthField()); //$NON-NLS-1$
+
+		}
+		return fieldPanel;
+	}
+	
+
+    void showUser(Long id) {
+        try {
+            bufferedId = id;
+            User user = parentFrame.getUserDao().find(id);
+            getFirstNameField().setText(user.getFirstName());
+            getLastNameField().setText(user.getLastName());
+            getDateOfBirthField().setText(DateFormat.getDateInstance().format(user.getDateOfBirth()));
+        } catch (DatabaseException e) {
+            JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            this.setVisible(false);
+            parentFrame.showBrowsePanel();
+        }
+    }
+
+}
+
+	
+
