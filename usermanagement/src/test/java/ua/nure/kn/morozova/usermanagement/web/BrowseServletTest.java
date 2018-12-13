9c7e67fe1d8148aed793ce7e2aa10d037ecf1f93
DashaMorozova <daria.morozova1@nure.ua>
2018-12-12 20:48:03 -1100

    Adds jars and creates new classes

---
+++ usermanagement/src/test/java/ua/nure/kn/morozova/usermanagement/web/BrowseServletTest.java
@@ -0,0 +1,40 @@
+package ua.nure.kn.morozova.usermanagement.web;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ua.nure.kn.morozova.usermanagement.User;
+
+public class BrowseServletTest extends MockServletTestCase {
+
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+        createServlet(BrowseServlet.class);
+	}
+
+	
+    @Test
+    public void testBrowse() {
+        User user = new User(new Long(1000),"Dasha", "Morozova", new Date());
+        List list = Collections.singletonList(user);
+        getMockUserDao().expectAndReturn("findAll", user);
+        doGet();
+        Collection collection = (Collection) getWebMockObjectFactory().getMockSession().getAttribute("users");
+        assertNotNull("Could not find list of users in session", collection);
+        assertSame(list, collection);
+    }
+    
+    
+    
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
